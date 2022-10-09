5b9af6972f6c029bde2f5f892c78874124c01f88
ThulsaDoom252 <xenolm252@gmail.com>
2022-10-09 17:58:31 +0300

    Footer component finished

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Profile from "./components/Profile/Profile";
 import Footer from "./components/Footer";
+import {BrowserRouter, Routes, Router, Route} from "react-router-dom";
 
 function App() {
   return (
+      <BrowserRouter>
       <div className="container">
           <Header/>
           <Sidebar/>
           <section className={"section-content"}>
-              <Profile/>
+              <Routes>
+                  <Route path={"/profile"} element={<Profile/>}/>
+              </Routes>
               <Footer/>
           </section>
       </div>
+      </BrowserRouter>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {BsFacebook, BsInstagram, BsTelegram} from "react-icons/bs";
 
-function Footer(props) {
-    return (
-        <div className={"footer"}>Footer
-        </div>
-    );
+const openUrl = (url) => {
+    window.open(url, "_blank");
 }
+
+const Footer = () => (
+    <div className={"footer"}>
+        <div className={"footer-container"}>
+            <BsInstagram
+                className={"footer-instagram-icon"}
+                onClick={() => {
+                    openUrl("https://www.instagram.com/t_h_u_l_s_a_d_o_o_m/")
+                }}/>
+            <BsFacebook
+                className={"footer-facebook-icon"}
+                onClick={() => {
+                    openUrl("https://www.facebook.com/xenolm252")
+                }}/>
+            <BsTelegram className={"footer-telegram-icon"}
+                        onClick={() => {
+                            openUrl("https://t.me/ThulsaDoom92")
+                        }}/>
+        </div>
+    </div>
+)
 
 export default Footer;
