c9892a53f1b2b32e4df30a0e1db6b0fa2837e496
ThulsaDoom252 <xenolm252@gmail.com>
2022-10-09 18:39:14 +0300

    add routes to App.js, navlinks & logo to Sidebar.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Sidebar from "./components/Sidebar";
 import Profile from "./components/Profile/Profile";
 import Footer from "./components/Footer";
 import {BrowserRouter, Routes, Router, Route} from "react-router-dom";
+import Settings from "./components/Settings";
+import News from "./components/News";
+import Users from "./components/Users";
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
           <section className={"section-content"}>
               <Routes>
                   <Route path={"/profile"} element={<Profile/>}/>
+                  <Route path={"/users"} element={<Users/>}/>
+                  <Route path={"/news"} element={<News/>}/>
+                  <Route path={"/settings"} element={<Settings/>}/>
               </Routes>
               <Footer/>
           </section>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+import logo from "./common/logo.png"
 
 function Sidebar(props) {
     return (
         <div className={"sidebar"}>
+            <img className={"sidebar-logo"} src={logo} alt='logo'/>
+            <NavLink className={navData => navData.isActive ? "sidebar-item-active" : "sidebar-item"} to={"/profile"}>Profile</NavLink>
+            <NavLink className={navData => navData.isActive ? "sidebar-item-active" : "sidebar-item"} to={"/users"}>Users</NavLink>
+            <NavLink className={navData => navData.isActive ? "sidebar-item-active" : "sidebar-item"} to={"/news"}>News</NavLink>
+            <NavLink className={navData => navData.isActive ? "sidebar-item-active" : "sidebar-item"} to={"/settings"}>Settings</NavLink>
         </div>
     );
 }
