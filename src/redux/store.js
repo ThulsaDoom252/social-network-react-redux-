f859b136f6302294a4ecd5545c64d1a21c025b69
ThulsaDoom252 <xenolm252@gmail.com>
2022-10-09 13:33:08 +0300

    add app-reducer & auth-reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,18 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunkMiddleware from "redux-thunk"
 import profileReducer from "./profile-reducer";
+import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
+import appReducer from "./app-reducer";
 
 let reducers = combineReducers({
-    profilePage: profileReducer
+    profilePage: profileReducer,
+    usersPage: usersReducer,
+    auth: authReducer,
+    app: appReducer,
 
 })
 
-const store = createStore(combineReducers, applyMiddleware(thunkMiddleware))
+const store = createStore(reducers, applyMiddleware(thunkMiddleware))
 
 export default store
