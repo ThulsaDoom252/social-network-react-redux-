bc1220736c65f929d61a51e7cd8431efd48fdf4e
ThulsaDoom252 <xenolm252@gmail.com>
2022-10-14 22:35:59 +0300

    header component completed. add burger-navbar

---
+++ src/components/BurgerNavbar.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import {NavLink} from "react-router-dom";
+import {GiHamburgerMenu} from "react-icons/gi";
+
+const BurgerNavbar = () => {
+    const [mobileNavBar, showMobileNavBar] = useState(false)
+
+    const showNavBar = () => {
+        showMobileNavBar(true)
+    }
+
+    const hideNavBar = () => {
+        showMobileNavBar(false)
+    }
+
+    return (
+        <div  className={"header-burger-navbar"}>
+            <button className={"header-burger-navbar-button"}
+                    onClick={!mobileNavBar ? showNavBar : hideNavBar}>
+                <GiHamburgerMenu/></button>
+            <div  hidden={!mobileNavBar} className={"header-burger-navbar-block"}>
+                <div className="header-burger-navbar-menu"></div>
+                <NavLink to={`/profile`}
+                         onClick={hideNavBar}
+                         className={navData => navData.isActive ? "burger-item-active" : "burger-item"}>Profile</NavLink>
+                <NavLink to="/users"
+                         onClick={hideNavBar}
+                         className={navData => navData.isActive ? "burger-item-active" : "burger-item"}> Users </NavLink>
+                <NavLink to="/news"
+                         onClick={hideNavBar}
+                         className={navData => navData.isActive ? "burger-item-active" : "burger-item"}> News </NavLink>
+                <NavLink to="/settings"
+                         onClick={hideNavBar}
+                         className={navData => navData.isActive ? "burger-item-active" : "burger-item"}> Settings </NavLink>
+            </div>
+        </div>
+    );
+}
+
+export default BurgerNavbar;
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import authHoc from "./HOC/authHoc";
 import {connect} from "react-redux";
+import {compose} from "redux";
+import {nightModeAC, underConstructionAC} from "../redux/app-reducer";
 
-function Settings(props) {
-    return (
-        <div>Settings</div>
-    );
+const Settings = (props) => {
+    return <div>
+        Settings
+    </div>
 }
 
-const mapStateToProps = (state) => {
+let settingsState = (state) => {
     return {
-        auth: state.auth.isLogged
+        auth: state.auth.isLogged,
+        underConstruction: state.app.underConstruction,
+        nightMode: state.app.nightMode,
+        nightModeColors: state.app.nightModeColors
+
     }
 }
 
-export default connect (mapStateToProps)(authHoc(Settings));
+
+export default compose(connect(settingsState, {nightModeAC, underConstructionAC}), authHoc)(Settings)
