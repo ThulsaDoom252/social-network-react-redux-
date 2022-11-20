816a4860805b4154530f526b17ce03a8bb9a0e70
ThulsaDoom252 <xenolm252@gmail.com>
2022-11-20 22:05:24 +0300

    add contact-inputs in profile-data component

--- src/components/Profile/ProfileInfo/profileData.js
+++ src/components/Profile/ProfileInfo/profileData.js
@@ -5,6 +5,7 @@ import * as Yup from 'yup'
 import {updateProfileTC} from "../../../redux/profile-reducer";
 import {BiMessageSquareAdd} from "react-icons/bi";
 import {AiOutlinePlusSquare} from "react-icons/all";
+import {NavLink} from "react-router-dom";
 
 const ProfileData = (props) => {
     const currentUser = props.currentUser
@@ -15,12 +16,21 @@ const ProfileData = (props) => {
     const [contactsEditMode, setContactsEditMode] = useState(false)
     const [youtubeLink, setYoutubeLink] = useState(props.profile.contacts.youtube)
     const [instagramLink, setInstagramLink] = useState(props.profile.contacts.instagram)
-    const [facebookLink, setFacebookLink] = useState(props.profile.contacts.facebook)
+    const [faceBookLink, setFaceBookLink] = useState(props.profile.contacts.facebook)
     const [mainLink, setMainLink] = useState(props.profile.contacts.mainLink)
     const [githubLink, setGithubLink] = useState(props.profile.contacts.github)
     const [vkLink, setVkLink] = useState(props.profile.contacts.vk)
     const [websiteLink, setWebsiteLink] = useState(props.profile.contacts.website)
     const [twitterLink, setTwitterLink] = useState(props.profile.contacts.twitter)
+    const [youTubeEditMode, setYouTubeEditMode] = useState(false)
+    const [instagramEditMode, setInstagramEditMode] = useState(false)
+    const [faceBookEditMode, setFaceBookEditMode] = useState(false)
+    const [mainLinkEditMode, setMainLinkEditMode] = useState(false)
+    const [gitHubEditMode, setGitHubEditMode] = useState(false)
+    const [vkEditMode, setVkEditMode] = useState(false)
+    const [webSiteEditMode, setWebSiteEditMode] = useState(false)
+    const [twitterEditMode, setTwitterEditMode] = useState(false)
+
     const contactsArray = [
         props.profile.contacts.youtube,
         props.profile.contacts.instagram,
@@ -33,6 +43,7 @@ const ProfileData = (props) => {
     ]
 
     const urlError = Yup.string().matches(/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, 'Enter correct url!').nullable()
+
     const formik = useFormik({
         initialValues: {
             name: props.profile.fullName,
@@ -41,18 +52,18 @@ const ProfileData = (props) => {
             jobDescription: props.profile.lookingForAJobDescription,
             website: websiteLink,
             vk: vkLink,
-            facebook: facebookLink,
+            faceBook: faceBookLink,
             twitter: twitterLink,
             instagram: instagramLink,
             gitHub: githubLink,
             mainLink: mainLink,
-            youtube: youtubeLink
+            youtube: props.profile.contacts.youtube
 
         },
         validationSchema: Yup.object({
-            name: Yup.string().min(3, 'Your name must be longer than 3 characters').required(),
-            about: Yup.string().min(3, 'Info must contain more than 3 characters!'),
-            jobDescription: Yup.string().min(3, 'Job description must contain more than 3 characters!').required(),
+            name: Yup.string().min(4, 'Your name must be longer than 3 characters').required(),
+            about: Yup.string().min(4, 'Info must contain more than 3 characters!').nullable(),
+            jobDescription: Yup.string().min(4, 'Job description must contain more than 3 characters!').nullable(),
             vk: urlError,
             facebook: urlError,
             instagram: urlError,
@@ -68,20 +79,16 @@ const ProfileData = (props) => {
     const toggleProfileDataEditMode = (editMode, setEditMode) => {
         if (editMode === false && isCurrentUser === true) {
             setEditMode(true)
-        } else if (editMode === true && !formik.errors.name && !formik.errors.about) {
+        } else if (editMode === true && !formik.errors.name && !formik.errors.about && !formik.errors.jobDescription) {
             setEditMode(false)
-            props.updateProfileTC(currentUser, aboutInfo, (jobInfo !== "enter job description"), jobInfo, formik.values.name, githubLink, vkLink, facebookLink, instagramLink,
-                twitterLink, websiteLink, youtubeLink, mainLink
+            props.updateProfileTC(currentUser, aboutInfo, (jobInfo !== "enter job description"), jobInfo, formik.values.name, githubLink, vkLink, faceBookLink, instagramLink,
+                twitterLink, websiteLink, formik.values.youtube, mainLink
             )
         }
     }
 
     let aboutInfo = formik.values.about === "" ? "no info" : formik.values.about
     let jobInfo = formik.values.jobDescription === "" ? "enter job description" : formik.values.jobDescription
-    window.isLook = formik.values.isLookingForAJob
-    window.description = formik.values.jobDescription
-    window.jobInfo = jobInfo
-
     const errorStyle = "profile-page-input-error"
 
     useEffect(() => {
@@ -92,14 +99,14 @@ const ProfileData = (props) => {
     }, [props.profile.fullName, props.profile.aboutMe, props.profile.lookingForAJob, props.profile.lookingForAJobDescription])
 
     useEffect(() => {
-        setYoutubeLink(contactsArray[0])
-        setInstagramLink(contactsArray[1])
-        setFacebookLink(contactsArray[2])
-        setMainLink(contactsArray[3])
-        setGithubLink(contactsArray[4])
-        setVkLink(contactsArray[5])
-        setWebsiteLink(contactsArray[6])
-        setTwitterLink(contactsArray[7])
+        formik.setFieldValue("youtube", contactsArray[0] === null ? "" : contactsArray[0])
+        setInstagramLink(contactsArray[1] === null ? "" : contactsArray[1])
+        setFaceBookLink(contactsArray[2] === null ? "" : contactsArray[2])
+        setMainLink(contactsArray[3] === null ? "" : contactsArray[3])
+        setGithubLink(contactsArray[4] === null ? "" : contactsArray[4])
+        setVkLink(contactsArray[5] === null ? "" : contactsArray[5])
+        setWebsiteLink(contactsArray[6] === null ? "" : contactsArray[6])
+        setTwitterLink(contactsArray[7] === null ? "" : contactsArray[7])
     }, contactsArray)
 
     return (
@@ -142,52 +149,99 @@ const ProfileData = (props) => {
             </div>
             <div className={"profile-page-job-info-block"}>
                 <p hidden={!formik.values.isLookingForAJob && isCurrentUser}>{!formik.values.isLookingForAJob && !isCurrentUser ? "Not looking for a job" : "Looking for a job"}</p>
+                <p className={errorStyle}>{formik.errors.jobDescription}</p>
                 {jobEditMode ? <input className="profile-page-input"
                                       id={"jobDescription"}
                                       autoFocus={true}
                                       onBlur={() => {
-                                          formik.values.jobDescription === "enter job description"  ?
+                                          formik.values.jobDescription === "enter job description" ?
                                               formik.setFieldValue("isLookingForAJob", false) && toggleProfileDataEditMode(jobEditMode, setJobEditMode) :
-                                              formik.values.jobDescription === ""  ? formik.setFieldValue("isLookingForAJob", false) && toggleProfileDataEditMode(jobEditMode, setJobEditMode) :
-                                              formik.setFieldValue("isLookingForAJob", true) && toggleProfileDataEditMode(jobEditMode, setJobEditMode)
+                                              formik.values.jobDescription === "" ? formik.setFieldValue("isLookingForAJob", false) && toggleProfileDataEditMode(jobEditMode, setJobEditMode) :
+                                                  formik.setFieldValue("isLookingForAJob", true) && toggleProfileDataEditMode(jobEditMode, setJobEditMode)
                                       }}
                                       type="text" value={formik.values.jobDescription}
                                       onChange={formik.handleChange}/>
                     :
-                    <p onDoubleClick={() => toggleProfileDataEditMode(jobEditMode, setJobEditMode)}>{formik.values.jobDescription === "" && isCurrentUser || formik.values.jobDescription === "enter job description" && isCurrentUser ? <span><AiOutlinePlusSquare/>Click to add job description</span> : formik.values.jobDescription}</p>}
+                    <p onDoubleClick={() => toggleProfileDataEditMode(jobEditMode, setJobEditMode)}>{formik.values.jobDescription === "" && isCurrentUser || formik.values.jobDescription === "enter job description" && isCurrentUser ?
+                        <span><AiOutlinePlusSquare/>Click to add job description</span> : formik.values.jobDescription}</p>}
 
             </div>
             <div className={"profile-page-contacts-info-block"}
-                 onDoubleClick={() => !contactsEditMode && toggleProfileDataEditMode(contactsEditMode, setContactsEditMode)}>
-                {contactsEditMode ?
-                    <div>
-                        <p><input className="profile-page-input" placeholder="youtube" type="text" value={youtubeLink}
-                                  onChange={(e) => setYoutubeLink(e.currentTarget.value)}/></p>
+                 onDoubleClick={() => !contactsEditMode && toggleProfileDataEditMode(contactsEditMode, setContactsEditMode)}
+                 onBlur={() => toggleProfileDataEditMode(contactsEditMode, setContactsEditMode)}>
+                <div>
+                    <p className={errorStyle}>{formik.errors.youtube}</p>
+                    {youTubeEditMode === true ?
+                        <p><input className="profile-page-input"
+                                  id={"youtube"}
+                                  placeholder="youtube" type="text"
+                                  value={formik.values.youtube}
+                                  onBlur={() => toggleProfileDataEditMode(youTubeEditMode, setYouTubeEditMode)}
+                                  autoFocus={true}
+                                  onChange={formik.handleChange}/></p> :
+                        <p onDoubleClick={() => toggleProfileDataEditMode(youTubeEditMode, setYouTubeEditMode)}>{formik.values.youtube === "" && isCurrentUser ?
+                            <span><AiOutlinePlusSquare/> Add Youtube</span> : formik.values.youtube}</p>
+                    }
+                    {instagramEditMode ?
                         <p><input className="profile-page-input" placeholder="instagram" type="text"
-                                  value={instagramLink} onChange={(e) => setInstagramLink(e.currentTarget.value)}/></p>
-                        <p><input className="profile-page-input" placeholder="facebook" type="text"
-                                  value={facebookLink} onChange={(e) => setFacebookLink(e.currentTarget.value)}/></p>
-                        <p><input className="profile-page-input" placeholder="mainLink" type="text"
-                                  value={mainLink} onChange={(e) => setMainLink(e.currentTarget.value)}/></p>
-                        <p><input className="profile-page-input" placeholder="github" type="text"
-                                  value={githubLink} onChange={(e) => setGithubLink(e.currentTarget.value)}/></p>
-                        <p><input className="profile-page-input" placeholder="vk" type="text"
-                                  value={vkLink} onChange={(e) => setVkLink(e.currentTarget.value)}/></p>
-                        <p><input className="profile-page-input" placeholder="website" type="text"
-                                  value={websiteLink} onChange={(e) => setWebsiteLink(e.currentTarget.value)}/></p>
-                        <p><input className="profile-page-input" placeholder="twitter" type="text"
-                                  value={twitterLink} onChange={(e) => setTwitterLink(e.currentTarget.value)}/></p>
-                    </div> :
-                    <div>
-                        <p>{youtubeLink}</p>
-                        <p>{instagramLink}</p>
-                        <p>{facebookLink}</p>
-                        <p>{mainLink}</p>
-                        <p>{githubLink}</p>
-                        <p>{vkLink}</p>
-                        <p>{websiteLink}</p>
-                        <p>{twitterLink}</p>
-                    </div>}
+                                  autoFocus={true}
+                                  onBlur={() => toggleProfileDataEditMode(instagramEditMode, setInstagramEditMode)}
+                                  value={instagramLink} onChange={(e) => setInstagramLink(e.currentTarget.value)}/>
+                        </p> :
+                        <p onDoubleClick={() => toggleProfileDataEditMode(instagramEditMode, setInstagramEditMode)}>{instagramLink === "" && isCurrentUser ?
+                            <span><AiOutlinePlusSquare/> Add Instagram</span> : instagramLink}</p>
+                    }
+                    {faceBookEditMode ? <p><input className="profile-page-input" placeholder="facebook" type="text"
+                                                  autoFocus={true}
+                                                  onBlur={() => toggleProfileDataEditMode(faceBookEditMode, setFaceBookEditMode)}
+                                                  value={faceBookLink}
+                                                  onChange={(e) => setFaceBookLink(e.currentTarget.value)}/>
+                        </p> :
+                        <p onDoubleClick={() => toggleProfileDataEditMode(faceBookEditMode, setFaceBookEditMode)}>{faceBookLink === "" && isCurrentUser ?
+                            <span><AiOutlinePlusSquare/> Add Facebook</span> : faceBookLink}</p>
+                    }
+
+                    {mainLinkEditMode ? <p><input className="profile-page-input" placeholder="mainLink" type="text"
+                                                  autoFocus={true}
+                                                  onBlur={() => toggleProfileDataEditMode(mainLinkEditMode, setMainLinkEditMode)}
+                                                  value={mainLink}
+                                                  onChange={(e) => setMainLink(e.currentTarget.value)}/></p> :
+                        <p onDoubleClick={() => toggleProfileDataEditMode(mainLinkEditMode, setMainLinkEditMode)}>{mainLink === "" && isCurrentUser ?
+                            <span><AiOutlinePlusSquare/> Add MainLink </span> : mainLink}</p>
+                    }
+                    {gitHubEditMode ? <p><input className="profile-page-input" placeholder="github" type="text"
+                                                autoFocus={true}
+                                                onBlur={() => toggleProfileDataEditMode(gitHubEditMode, setGitHubEditMode)}
+                                                value={githubLink}
+                                                onChange={(e) => setGithubLink(e.currentTarget.value)}/></p> :
+                        <p onDoubleClick={() => toggleProfileDataEditMode(gitHubEditMode, setGitHubEditMode)}>{githubLink === "" && isCurrentUser ?
+                            <span><AiOutlinePlusSquare/> Add GitHub</span> : githubLink}</p>
+                    }
+                    {vkEditMode ? <p><input className="profile-page-input" placeholder="vk" type="text"
+                                            autoFocus={true}
+                                            onBlur={() => toggleProfileDataEditMode(vkEditMode, setVkEditMode)}
+                                            value={vkLink} onChange={(e) => setVkLink(e.currentTarget.value)}/></p> :
+                        <p onDoubleClick={() => toggleProfileDataEditMode(vkEditMode, setVkEditMode)}>{vkLink === "" && isCurrentUser ?
+                            <span><AiOutlinePlusSquare/>Add Vk</span> : vkLink}</p>
+                    }
+                    {webSiteEditMode ? <p><input className="profile-page-input" placeholder="website" type="text"
+                                                 autoFocus={true}
+                                                 onBlur={() => toggleProfileDataEditMode(webSiteEditMode, setWebSiteEditMode)}
+                                                 value={websiteLink}
+                                                 onChange={(e) => setWebsiteLink(e.currentTarget.value)}/></p> :
+                        <p onDoubleClick={() => toggleProfileDataEditMode(webSiteEditMode, setWebSiteEditMode)}>{websiteLink === "" && isCurrentUser ?
+                            <span><AiOutlinePlusSquare/>Add website</span> : websiteLink}</p>
+                    }
+                    {twitterEditMode ? <p><input className="profile-page-input" placeholder="twitter" type="text"
+                                                 autoFocus={true}
+                                                 onBlur={() => toggleProfileDataEditMode(twitterEditMode, setTwitterEditMode)}
+                                                 value={twitterLink}
+                                                 onChange={(e) => setTwitterLink(e.currentTarget.value)}/></p> :
+
+                        <p onDoubleClick={() => toggleProfileDataEditMode(twitterEditMode, setTwitterEditMode)}>{twitterLink === "" && isCurrentUser ?
+                            <span><AiOutlinePlusSquare/>Add twitter</span> : twitterLink}</p>
+                    }
+                </div>
             </div>
         </form>
     );
